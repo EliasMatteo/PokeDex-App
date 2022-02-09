291f8bfb69b553850bc1bcf64f44b6dff42c307e
Amelie <peach.panja@gmail.com>
2022-02-08 20:19:40 -0700

    Search bar and data mapping completed

--- my-poggydex/pages/api/pokemon.js
+++ my-poggydex/pages/api/pokemon.js
@@ -1,7 +1,11 @@
+import { filtering } from "../../utilities/functions.js";
 import pokemon from "../../utilities/pokedex.json";
 
-export default function handler(req, res) {
-  // for fast testing
-  const pokemons = pokemon.slice(0, 5);
-  res.json(pokemons);
+export default async function handler(req, res) {
+  const { name } = req.query;
+
+  let lists = filtering(pokemon, {
+    name: name,
+  });
+  res.status(200).json(lists);
 }
--- my-poggydex/pages/index.js
+++ my-poggydex/pages/index.js
@@ -1,36 +1,68 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Image from "next/image";
 
 // import components
 import PokedexCardCont from "../components/pokedexCardCont";
 
 export default function Home() {
-  // empty data array
-  const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+  const [pokemons, setPokemons] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+
   useEffect(() => {
-    const getPokemon = async () => {
-      // use axios to fetch api
-      const res = await axios.get("/api/pokemon");
-      // set data
-      setData(res.data);
+    if (!isSearching) return;
+
+    let cancelSetPokemons = false;
+    const getPokemons = async () => {
+      const res = await axios.get("/api/pokemon", {
+        params: {
+          name: name,
+        },
+      });
+      if (!cancelSetPokemons) {
+        setPokemons(res.data);
+        setIsSearching(false);
+      }
     };
-    // call function
-    getPokemon();
-  }, []);
+
+    getPokemons();
+
+    if (isSearching) return () => (cancelSetPokemons = true);
+  }, [isSearching, setIsSearching, name]);
+
   return (
     <div>
+      <div className="search-bar">
+        <input
+          className="searh-bar-text-box"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Pokemon Name"
+        />
+        <button
+          className="search-bar-button"
+          onClick={() => setIsSearching(true)}
+        >
+          <Image
+            src={"/icon/search.svg"}
+            alt="search icon"
+            width={20}
+            height={20}
+          />
+        </button>
+      </div>
       {/* data mapping */}
       <div className="index-pokedex-card-display">
-        {data.map((pokemon) => (
-          // use name as unique id
-          <div key={pokemon.name}>
-            <PokedexCardCont
-              pokemonname={pokemon.name}
-              type1={pokemon.type_1}
-              type2={pokemon.type_2}
-            />
-          </div>
-        ))}
+        {pokemons.length > 0 &&
+          pokemons.map((pokemon) => (
+            <div key={pokemon.name}>
+              <PokedexCardCont
+                pokemonname={pokemon.name}
+                type1={pokemon.type_1}
+                type2={pokemon.type_2}
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
