dc20fe83abd2fd6ddda42f1c4239d44d1b1a9535
EliasMatteo <eflamencosolito@my.bcit.ca>
2022-02-11 16:49:39 -0800

    base commit

---
+++ my-poggydex/components/pokemonEvoluton.js
@@ -0,0 +1,21 @@
+
+
+const pokemonEvolution = (
+    evolutionName,
+    
+) => {
+    return <div className="evolution-card-cont">
+        <div className="evolution-image-cont">
+            <image src="" width={184} height={166}></image>
+        </div>
+        <h1 className="pokemon-evolution-text">
+            {evolutionName} 
+        </h1>
+
+
+    </div>
+
+
+    };
+
+    export default pokemonEvolution;
