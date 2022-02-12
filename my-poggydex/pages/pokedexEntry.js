f2d2fe9aecc97d9c51345e0c54a3db75011da261
EliasMatteo <eflamencosolito@my.bcit.ca>
2022-02-11 17:01:20 -0800

    added pokedex entry page

---
+++ my-poggydex/pages/pokedexEntry.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Image from "next/image";
+
+import pokemonEvolution from "../components/pokemonEvolution";
+
+export default function pokemonEntry() {
+  return <></>;
+}
