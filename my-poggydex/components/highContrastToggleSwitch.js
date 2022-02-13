5a009bed1da8bca4d810294cf7514415ca57a98a
EliasMatteo <eflamencosolito@my.bcit.ca>
2022-02-13 00:24:34 -0800

    created "switch" comps for settings page and added styling files alongside icons

---
+++ my-poggydex/components/highContrastToggleSwitch.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+const HighContrastToggleSwitch = ({
+  isContrastChecked,
+  handleContrastToggle,
+}) => {
+  console.log(isContrastChecked);
+  return (
+    <div className="switch-cont">
+      <input
+        checked={isContrastChecked}
+        onChange={handleContrastToggle}
+        type="checkbox"
+        className="switch-checkbox"
+        id="contrast-switch-toggle"
+      />
+      <label className="switch-label" htmlFor="contrast-switch-toggle">
+        <span className="switch-button">
+          {isContrastChecked == true ? (
+            <Image
+              src={"/icon/highContrastTextIconDarkMode.svg"}
+              width={20}
+              height={20}
+              alt="icon"
+            />
+          ) : (
+            <Image
+              src={"/icon/highContrastTextIconLightMode.svg"}
+              width={20}
+              height={20}
+              alt="icon"
+            />
+          )}
+        </span>
+      </label>
+    </div>
+  );
+};
+
+export default HighContrastToggleSwitch;
--- my-poggydex/components/themeToggleSwitch.js
+++ my-poggydex/components/themeToggleSwitch.js
@@ -1,5 +1,37 @@
-const ThemeToggleSwitch = () => {
-  return <></>;
+import Image from "next/image";
+
+const ThemeToggleSwitch = ({ isThemeChecked, handleThemeToggle }) => {
+  console.log(isThemeChecked);
+  return (
+    <div className="switch-cont">
+      <input
+        checked={isThemeChecked}
+        onChange={handleThemeToggle}
+        type="checkbox"
+        className="switch-checkbox"
+        id="theme-switch-toggle"
+      />
+      <label className="switch-label" htmlFor="theme-switch-toggle">
+        <span className="switch-button">
+          {isThemeChecked == true ? (
+            <Image
+              src={"/icon/themeToggleDarkMode.svg"}
+              width={35}
+              height={40}
+              alt="icon"
+            />
+          ) : (
+            <Image
+              src={"/icon/themeToggleLightMode.svg"}
+              width={40}
+              height={40}
+              alt="icon"
+            />
+          )}
+        </span>
+      </label>
+    </div>
+  );
 };
 
 export default ThemeToggleSwitch;
