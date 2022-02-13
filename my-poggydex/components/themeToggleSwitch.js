62d561f501a8d97e9f003c22aa78a130525f2342
EliasMatteo <eflamencosolito@my.bcit.ca>
2022-02-12 21:19:09 -0800

    created and started "ThemeToggleSwitch", made "settings" page

---
+++ my-poggydex/components/themeToggleSwitch.js
@@ -0,0 +1,5 @@
+const ThemeToggleSwitch = () => {
+  return <></>;
+};
+
+export default ThemeToggleSwitch;
---
+++ my-poggydex/pages/settings.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import ThemeToggleSwitch from "../components/themeToggleSwitch";
+
+export default function Settings() {
+  return (
+    <div>
+      <ThemeToggleSwitch />
+    </div>
+  );
+}
