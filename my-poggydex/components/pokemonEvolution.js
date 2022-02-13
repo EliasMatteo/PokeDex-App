f12cf843ec275af4fe618236b8edb66a8fa21a0f
EliasMatteo <eflamencosolito@my.bcit.ca>
2022-02-12 20:58:21 -0800

    created "pokedexEvolution" comp and styling files, made "pokedexEntry" page
    created "pokemonEvolution" component and added relevant styling files and assets, also created the "pokedexEntry" page for the placemnt of components

--- my-poggydex/components/pokemonEvolution.js
+++ my-poggydex/components/pokemonEvolution.js
@@ -1,21 +1,32 @@
+import axios from "axios";
+import Image from "next/image";
 
-
-const pokemonEvolution = (
-    evolutionName,
-    
-) => {
-    return <div className="evolution-card-cont">
-        <div className="evolution-image-cont">
-            <image src="" width={184} height={166}></image>
+const PokemonEvolution = ({ pokemonname = "henry", arrowDisplay = null }) => {
+  return (
+    <div className="evolution-card-cont">
+      <div className="evolution-image-cont">
+        <div className="evolution-image-icon">
+          <Image
+            src={`/pokemon/${pokemonname}.png`}
+            width={184}
+            height={184}
+            alt={pokemonname}
+            className="poke-img"
+          />
+          {arrowDisplay == "show" ? (
+            <Image
+              src={"/icon/arrowDown.svg"}
+              alt="arrow icon"
+              width={30}
+              height={30}
+              className="arrow-icon"
+            />
+          ) : null}
         </div>
-        <h1 className="pokemon-evolution-text">
-            {evolutionName} 
-        </h1>
-
-
+        <h1 className="pokemon-evolution-text">{pokemonname}</h1>
+      </div>
     </div>
+  );
+};
 
-
-    };
-
-    export default pokemonEvolution;
+export default PokemonEvolution;
--- my-poggydex/pages/pokedexEntry.js
+++ my-poggydex/pages/pokedexEntry.js
@@ -2,8 +2,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
 
-import pokemonEvolution from "../components/pokemonEvolution";
+import PokemonEvolution from "../components/pokemonEvolution";
 
 export default function pokemonEntry() {
-  return <></>;
+  return (
+    <div className="pokemonEntry-evolution-display">
+      <PokemonEvolution pokemonname="bulbasaur" arrowDisplay="show" />
+      <PokemonEvolution pokemonname="ivysaur" arrowDisplay="show" />
+      <PokemonEvolution pokemonname="venusaur" arrowDisplay={null} />
+    </div>
+  );
 }
