d37d59f252cb2e76523c6bfadf54bafa749c81f5
TylerD2K <tdo45@my.bcit.ca>
2022-02-17 00:53:41 -0800

    Trying to integrate other components

--- my-poggydex/components/infoTab.js
+++ my-poggydex/components/infoTab.js
@@ -1,4 +1,5 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import StatValue from './statValue';
 
 const InfoTab = () => (
   <Tabs>
@@ -9,7 +10,26 @@ const InfoTab = () => (
     </TabList>
 
     <TabPanel>
-      <p>Spaghetti</p>
+      <div className='container'>
+        <div className='left'>
+          <h2>About</h2>
+            <StatValue category={"Generation"} value={"Seed"}/>
+            <StatValue category={"Status"} value={"Seed"}/>
+            <StatValue category={"Species"} value={"Seed"}/>
+            <StatValue category={"Height"} value={"Seed"}/>
+            <StatValue category={"Weight"} value={"Seed"}/>
+            <StatValue category={"Abilities"} value={"Seed"}/>
+        </div>
+        <div className='right'>
+          <h2>Training</h2>
+            <StatValue category={"Catch Rate"} value={"Seed"}/>
+            <StatValue category={"Base Friendship"} value={"Seed"}/>
+            <StatValue category={"Base Experience"} value={"Seed"}/>
+            <StatValue category={"Growth Rate"} value={"Seed"}/>
+            <StatValue category={"Egg Type"} value={"Seed"}/>
+            <StatValue category={"Egg Cycle"} value={"Seed"}/>
+        </div>
+      </div>
     </TabPanel>
     <TabPanel>
       <p>Fettucini</p>
