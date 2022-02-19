dcf86ae3687f4ebd3796822bd3f2b7a1cb36f42b
TylerD2K <72163659+TylerD2K@users.noreply.github.com>
2022-02-18 16:39:39 -0800

    Figuring out heart icon

--- my-poggydex/components/favHeartIcon.js
+++ my-poggydex/components/favHeartIcon.js
@@ -1,16 +1,36 @@
 import Image from "next/image";
+import React, {useState} from 'react';
 
-const FavHeartIcon = ({}) => {
+const FavHeartIcon = ({
+    onIconInteract=()=>{}
+}) => {
+    const [favColor, setFavColor] = useState(false);
+
+    if(favColor === false) {
+        return (
+            <div className="fav-heart">
+                <Image
+                    src={"/../public/icon/heart.svg"}
+                    alt="favorite"
+                    width={20}
+                    height={20}>
+                </Image>
+            </div>
+        );
+    }
+
+    else if(favColor === true) {
     return (
         <div className="fav-heart">
             <Image
-                src={"/../public/icon/heart.svg"}
+                src={"/../public/icon/heartfilled.svg"}
                 alt="favorite"
                 width={20}
                 height={20}>
             </Image>
         </div>
-    );
-};
+        );
+    };
+}
 
 export default FavHeartIcon
