ceb8c114ca92cc83e74ecb9c7cbe3538d26d7421
Amelie <peach.panja@gmail.com>
2022-02-18 20:59:39 -0700

    clean up formatting

--- my-poggydex/components/infoTab.js
+++ my-poggydex/components/infoTab.js
@@ -1,10 +1,9 @@
-const InfoTab = ({name}) => {
-    return (
-      <button className="info-tab">
-        <h3>{name}</h3>
-      </button>
-    );
-  };
-  
-  export default InfoTab;
-  
+const InfoTab = ({ name }) => {
+  return (
+    <button className="info-tab">
+      <h3>{name}</h3>
+    </button>
+  );
+};
+
+export default InfoTab;
