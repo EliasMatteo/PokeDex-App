2a92a0fef0004e0e426ff742ec0b588330d7554b
nguyenjunn <nguyenjunn@gmail.com>
2022-02-20 03:12:38 -0800

    wrapped app provider

--- my-poggydex/pages/_app.js
+++ my-poggydex/pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.scss";
+import MyThemeProvider from "../utilities/provider";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MyThemeProvider>
+      <Component {...pageProps} />
+    </MyThemeProvider>
+  );
 }
 
 export default MyApp;
--- my-poggydex/pages/settings.js
+++ my-poggydex/pages/settings.js
@@ -2,15 +2,18 @@ import { useEffect, useState } from "react";
 import BoldTextToggleSwitch from "../components/boldTextToggleSwitch";
 import HighContrastToggleSwitch from "../components/highContrastToggleSwitch";
 import ThemeToggleSwitch from "../components/themeToggleSwitch";
+import { useTheme } from "../utilities/provider";
 
 export default function Settings() {
   const [switchThemeState, setSwitchThemeState] = useState(false);
   const [switchBoldState, setSwitchBoldState] = useState(false);
   const [switchContrastState, setSwitchContrastState] = useState(false);
+  const { setTheme } = useTheme();
 
   return (
     <div>
       <ThemeToggleSwitch
+        onClick={() => setTheme("dark")}
         isThemeChecked={switchThemeState}
         handleThemeToggle={() => setSwitchThemeState(!switchThemeState)}
       />
