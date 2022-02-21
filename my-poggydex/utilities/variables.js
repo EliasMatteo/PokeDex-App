0eefef1cd5b110aae0855ed50efabc424c21a3d5
nguyenjunn <nguyenjunn@gmail.com>
2022-02-20 19:43:22 -0800

    Just added some things to variables

--- my-poggydex/utilities/variables.js
+++ my-poggydex/utilities/variables.js
@@ -5,4 +5,16 @@ export const themes = {
   dark: {
     body: "#151515",
   },
+  bold: {
+    text: bold,
+  },
+  regular: {
+    text: regular,
+  },
+  contrast: {
+    text: "#151515",
+  },
+  normal: {
+    text: "#FFFFFF",
+  },
 };
