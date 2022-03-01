d0a62b97e8b4ab9e99c6daffbbb67b51de203eba
nguyenjunn <nguyenjunn@gmail.com>
2022-03-01 03:32:13 -0800

    created toggle for pokemon type/gen and added styling

--- my-poggydex/pages/settings.js
+++ my-poggydex/pages/settings.js
@@ -5,59 +5,99 @@ import HighContrastToggleSwitch from "../components/highContrastToggleSwitch";
 import ThemeToggleSwitch from "../components/themeToggleSwitch";
 import { useTheme } from "../utilities/provider";
 import Image from "next/image";
+import PokemonGen from "../components/pokemonGen";
+import PokemonType from "../components/pokemonType";
 
 export default function Settings() {
   const [switchThemeState, setSwitchThemeState] = useState(false);
   const [switchBoldState, setSwitchBoldState] = useState(false);
   const [switchContrastState, setSwitchContrastState] = useState(false);
   const { setTheme } = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenType, setIsOpenType] = useState(false);
 
   return (
     <div className="cont-settings">
-      <div className="button-cont">
-        <Link href={"/"}>
-          <Image src="/arrow-left.png" alt="back" width={40} height={20} />
-        </Link>
-        <Link href={"/"}>
-          <button>back to home</button>
-        </Link>
+      <div className="test-cont">
+        <div className="button-cont">
+          <Link href={"/"}>
+            <Image src="/arrow-left.png" alt="back" width={40} height={20} />
+          </Link>
+          <Link href={"/"}>
+            <button>back to home</button>
+          </Link>
+        </div>
+        <div className="cont-toggle">
+          <h2>Theme</h2>
+          <ThemeToggleSwitch
+            onClick={() => setTheme("dark")}
+            isThemeChecked={switchThemeState}
+            handleThemeToggle={() => setSwitchThemeState(!switchThemeState)}
+          />
+        </div>
+        <div className="cont-toggle">
+          <h2>Bolded Text</h2>
+          <BoldTextToggleSwitch
+            isBoldChecked={switchBoldState}
+            handleBoldToggle={() => setSwitchBoldState(!switchBoldState)}
+          />
+        </div>
+        <div className="cont-toggle">
+          <h2>High Contrast</h2>
+          <HighContrastToggleSwitch
+            isContrastChecked={switchContrastState}
+            handleContrastToggle={() =>
+              setSwitchContrastState(!switchContrastState)
+            }
+          />
+        </div>
+
+        <div className="cont-toggle">
+          <div className="pop-up-cont">
+            <button
+              className="filter-toggle"
+              onClick={() => setIsOpenType(!isOpenType)}
+            >
+              <h2>Pokemon Types</h2>
+            </button>
+
+            {isOpenType && (
+              <div className="content">
+                {" "}
+                <PokemonType />{" "}
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="cont-toggle">
+          <div className="pop-up-cont">
+            <button
+              className="filter-toggle"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <h2>Pokemon Generation</h2>
+            </button>
+
+            {isOpen && (
+              <div className="content">
+                {" "}
+                <PokemonGen />
+                <PokemonGen />
+                <PokemonGen />
+                <PokemonGen />
+                <PokemonGen />
+                <PokemonGen />{" "}
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-      <div className="cont-toggle">
-        <h2>Theme</h2>
-        <ThemeToggleSwitch
-          onClick={() => setTheme("dark")}
-          isThemeChecked={switchThemeState}
-          handleThemeToggle={() => setSwitchThemeState(!switchThemeState)}
-        />
-      </div>
-      <div className="cont-toggle">
-        <h2>Bolded Text</h2>
-        <BoldTextToggleSwitch
-          isBoldChecked={switchBoldState}
-          handleBoldToggle={() => setSwitchBoldState(!switchBoldState)}
-        />
-      </div>
-      <div className="cont-toggle">
-        <h2>High Contrast</h2>
-        <HighContrastToggleSwitch
-          isContrastChecked={switchContrastState}
-          handleContrastToggle={() =>
-            setSwitchContrastState(!switchContrastState)
-          }
-        />
-      </div>
-      <div className="cont-toggle">
-        <button className="filter-cont">
-          <h2>Pokemon Type</h2>
-        </button>
-      </div>
-      <div className="cont-toggle">
-        <button className="filter-cont">
-          <h2>Pokemon Generation</h2>
-        </button>
-      </div>
-      <div className="pokeball-setting-bg">
-        <Image src="/pokeball.png" alt="pokeball" width={500} height={500} />
+
+      <div className="pokeball-cont">
+        <div className="pokeball-setting-bg">
+          <Image src="/pokeball.svg" alt="pokeball" width={800} height={800} />
+        </div>
       </div>
     </div>
   );
