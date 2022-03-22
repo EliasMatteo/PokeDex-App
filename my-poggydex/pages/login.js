c11441e5bb2c1d8674187fb7d0aafd709cdd9f32
nguyenjunn <nguyenjunn@gmail.com>
2022-03-22 02:02:10 -0700

    register passes through

--- my-poggydex/pages/login.js
+++ my-poggydex/pages/login.js
@@ -1,32 +1,47 @@
 import Image from "next/image";
 import { useState } from "react";
 import PropTypes from "prop-types";
+// import { useHistory } from "react-router-dom";
 
-async function loginUser(credentials) {
-  return fetch("http://localhost:5000/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(credentials),
-  }).then((data) => data.json());
-}
+// async function loginUser(credentials) {
+//   //   return fetch("http://localhost:5000/signup", {
+//   //     method: "POST",
+//   //     headers: {
+//   //       "Content-Type": "application/json",
+//   //     },
+//   //     body: JSON.stringify(credentials),
+//   //   }).then((data) => data.json());
+// }
 
 export default function LogIn() {
   const [credentials, setCredentials] = useState("login");
-  const [username, setUserName] = useState();
-  const [password, setPassword] = useState();
-  const [email, setEmail] = useState();
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [passwordconfirmation, setPasswordCom] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const credentials = await loginUser({
-      username,
-      password,
-      email,
+  async function registerUser(event) {
+    event.preventDefault();
+    const response = await fetch("http://localhost:5000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/JSON",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+        passwordconfirmation,
+      }),
     });
-    setCredentials(credentials);
-  };
+
+    const data = await response.json();
+    console.log(data);
+    // if (data.status === "ok") {
+    //   history.push("/login");
+    // }
+  }
+
   return (
     <div className="login">
       <div className="login-title">
@@ -38,7 +53,7 @@ export default function LogIn() {
       </div>
       {credentials === "login" && (
         <div className="login-credentials">
-          <form className="login-credentials" onSubmit={handleSubmit}>
+          <form className="login-credentials">
             <input
               id="email"
               type="email"
@@ -72,9 +87,10 @@ export default function LogIn() {
       )}
       {credentials === "signup" && (
         <div className="login-credentials">
-          <form className="login-credentials" onSubmit={handleSubmit}>
+          <form className="login-credentials" onSubmit={registerUser}>
             <input
               id="username"
+              value={username}
               type="text"
               onChange={(e) => setUserName(e.target.value)}
               placeholder="Username"
@@ -83,6 +99,7 @@ export default function LogIn() {
             />
             <input
               id="email"
+              value={email}
               type="email"
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Email"
@@ -91,18 +108,32 @@ export default function LogIn() {
             />
             <input
               id="password"
+              value={password}
               type="password"
               onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
               required
               className="login-credentials-fields"
             />
+
+            <input
+              id="passwordconfirmation"
+              value={passwordconfirmation}
+              type="password"
+              onChange={(e) => setPasswordCom(e.target.value)}
+              placeholder="Password Confirmation"
+              required
+              className="login-credentials-fields"
+            />
+
+            <input
+              type="submit"
+              value="Register"
+              className="login-credentials-buttons"
+            />
           </form>
-          <button type="button" className="login-credentials-buttons">
-            Register
-          </button>
           <button
-            type="submit"
+            type="button"
             onClick={() => {
               setCredentials("login");
             }}
@@ -120,6 +151,6 @@ export default function LogIn() {
   );
 }
 
-LogIn.propTypes = {
-  setToken: PropTypes.func.isRequired,
-};
+// LogIn.propTypes = {
+//   setToken: PropTypes.func.isRequired,
+// };
